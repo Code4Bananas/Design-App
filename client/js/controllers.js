8b971c1fd1622f3328004b8d725ea1166ee6c36f
Code4Bananas <mcspc96@hotmail.com>
2017-06-10 20:47:32 -0700

    Create controllers.js

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -59,9 +59,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('OneCtrl', function($scope, $location) {
-  $scope.one = function(){
-    $location.path("/one");
-  }
+  
 })
 
 .controller('TwoCtrl', function($scope, $location) {
