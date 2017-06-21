02f20a8384b05ef5762fda8f7402b24f07b3d227
Code4Bananas <mcspc96@hotmail.com>
2017-06-21 13:33:28 -0700

    Update controllers.js

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ui.router])
 
 .controller('AppCtrl', function($scope, $location, RegistrationService) {
   $scope.logout = function() {
