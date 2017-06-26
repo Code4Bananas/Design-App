b7eb6ee21f1a3c33ab4f436ca3f0ff21c805707a
Code4Bananas <mcspc96@hotmail.com>
2017-06-26 09:55:49 -0700

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -152,7 +152,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   
   
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/index/home');
 
   // Register middleware to ensure our auth token is passed to the server
   $httpProvider.interceptors.push('TokenInterceptor');
